b479ef7a580c443f14c4e924a5c1f6469a88810e
Andreas Gropp <starmaerker@googlemail.com>
2019-01-07 16:44:48 +0100

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,12 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>It's time to conquer the world!</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.robinwieruch.de"
             target="_blank"
             rel="noopener noreferrer"
           >
